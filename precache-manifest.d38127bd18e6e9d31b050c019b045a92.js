d71488a3a10b0deed116cd5cbfa9c5a7df6ee898
Reno DuBois <renodubois@users.noreply.github.com>
2019-05-19 00:14:12 -0500

    Updates

---
+++ precache-manifest.d38127bd18e6e9d31b050c019b045a92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5cd427ad25f5bf7398d68f4717d3fd2",
+    "url": "/test-site/index.html"
+  },
+  {
+    "revision": "7a7c19b614a17c51ecb7",
+    "url": "/test-site/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c44b846c37ea496b6fcd",
+    "url": "/test-site/static/js/2.34b4fde6.chunk.js"
+  },
+  {
+    "revision": "7a7c19b614a17c51ecb7",
+    "url": "/test-site/static/js/main.4135d7dd.chunk.js"
+  },
+  {
+    "revision": "417e2bf747497162fabb",
+    "url": "/test-site/static/js/runtime~main.a617ddd9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-site/static/media/logo.5d5d9eef.svg"
+  }
+]);
